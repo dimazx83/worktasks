529ba54bef66cde53d3631e6836b93047d374b55
Dzmitry Piskunenka <dpiskunenko@xbsoftware.by>
2021-01-12 18:31:33 +0300

    fix

--- src/Stores/MainStore.js
+++ src/Stores/MainStore.js
@@ -7,7 +7,7 @@ import { FooterStore } from "./FooterStore.js";
 export class MainStore {
   constructor() {
     (this.headerStore = new HeaderStore()),
-    (this.itemsStore = new ItemsStore(): ItemsStore),
-    (this.footerStore = new FooterStore(): FooterStore);
+    (this.itemsStore = new ItemsStore()),
+    (this.footerStore = new FooterStore());
   }
 }
